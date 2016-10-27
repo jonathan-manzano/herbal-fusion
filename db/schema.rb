a2f4af4c64f6d46ae9bc2b49508dd5f6850ec152
alejandro <realdeal@Vishals-MacBook-Pro.local>
2016-10-26 18:45:03 -0700

    Deleted Like model from previous commit and regenerated it but with references

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161026184640) do
+ActiveRecord::Schema.define(version: 20161027014202) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,16 @@ ActiveRecord::Schema.define(version: 20161026184640) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "likes", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "recipe_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "likes", ["recipe_id"], name: "index_likes_on_recipe_id", using: :btree
+  add_index "likes", ["user_id"], name: "index_likes_on_user_id", using: :btree
 
   create_table "recipes", force: true do |t|
     t.text     "description"
