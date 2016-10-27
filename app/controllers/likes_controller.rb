c9fcbcff2f379bb8d8ba133090c607889eb9c25d
alejandro <realdeal@Vishals-MacBook-Pro.local>
2016-10-26 18:58:00 -0700

    created methods in likes controller and setup routes for likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,12 @@
 class LikesController < ApplicationController
+  def create
+    recipe = Recipe.find(params['recipe_id'])
+    like = Like.create(user: current_user, recipe: recipe)
+    redirect_to :back
+  end
+
+  def destroy
+    like = Like.find(params[:id]).destroy
+    redirect_to :back
+  end
 end
