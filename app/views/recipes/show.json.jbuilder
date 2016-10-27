bcaf75981e904c8e4eb87ababd07c7008c1e8971
Jagdeep <punjabi408@gmail.com>
2016-10-27 00:20:47 +0000

    added image

--- app/views/recipes/show.json.jbuilder
+++ app/views/recipes/show.json.jbuilder
@@ -1,1 +1,1 @@
-json.extract! @pin, :description, :created_at, :updated_at
+json.extract! @recipe, :description, :created_at, :updated_at
--- db/migrate/20131205211746_create_recipes.rb
+++ db/migrate/20131205211746_create_recipes.rb
@@ -3,10 +3,10 @@ class CreateRecipes < ActiveRecord::Migration
     create_table :recipes do |t|
     t.text    :description
     t.text     :steps
-    t.string     :title
-    t.integer     :time
+    t.string   :title
+    t.integer  :time
     t.integer  :serving_size
-    t.string     :allergies
+    t.string   :allergies
     t.text     :ingredients
 
     t.timestamps
