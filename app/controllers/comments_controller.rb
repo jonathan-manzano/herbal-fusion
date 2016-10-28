10035b880f1f1edd9722a5c6a0954d5aafb92f50
adamaly831 <AdamAly831@gmail.com>
2016-10-28 10:08:35 -0700

    final css changes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,22 +1,21 @@
 class CommentsController < ApplicationController
   def create
     @comment = Comment.create(content:params[:content], user:current_user, recipe_id:params[:recipe_id])
-    @recipe = Recipe.find(params['recipe_id'])
-
+    recipe_id = params[:recipe_id]
+    @recipe = Recipe.find(params["recipe_id"])
     redirect_to :back
   end
 
   def show
     @comment = User.find([:id]).comments.all
     @comment2 = User.find([:id]).comments.find(params[:comment_id])
-    
     puts @comment2
   end
 
   def destroy
     @comment = Comment.find(params[:id]).destroy
     puts @comment
-    
+
     redirect_to :back
   end
 end
